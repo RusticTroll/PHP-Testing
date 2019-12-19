4a3421ec0214cda0bb866472aeb946c8186e63a5
RTCfrc5902 <57148512+RTCfrc5902@users.noreply.github.com>
2019-12-19 13:08:00 -0500

    testing file added

---
+++ login-test.php
@@ -0,0 +1,23 @@
+<?php
+	session_start();
+    $server = "localhost";
+	$username = "root";
+	$password = "RgthEpJFq6sQ";
+	$db = "user_info";
+
+	// create connection
+	$conn = new mysqli($server, $username, $password, $db);
+
+	if ($conn->connect_error) {
+		die("Connection Failed: " . $conn->connection_error);
+    }
+    
+    $sanitizer = $conn->prepare("SELECT passwordHash FROM account_credentials WHERE username = :user");
+
+    $user = $_POST["username"];
+	$password = $_POST["password"];
+    $sanitizer->bind_param(':user', $user);
+
+	$_SESSION['messageID'] = $conn->query("IF OBJECT_ID('account_credentials', 'u') IS NOT NULL");
+		
+?>
--- messageboard.php
+++ messageboard.php
@@ -22,7 +22,9 @@
 		<div class="row">
 			<div class="col-12 text-center">
 				<?php 
-				$conn 
+				session_start();
+				
+				$_SESSION[]
 				
 				
 				?>
